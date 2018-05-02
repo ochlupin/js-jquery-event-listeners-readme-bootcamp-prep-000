b2e471877f85516067a6dd02f0d8821cc2d3d32f
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-05-02 20:09:43 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -16,11 +16,13 @@ $('input').on('keydown',function(key){
   if (key.which===71){
     alert("g was pressed");
   }
-})  
+})
 }
 
 function submitIt(){
-
+$('form').on('submit',function(){
+  alert ("Your form is going to be submitted now.")
+})
 }
 
 $(document).ready(function(){
